b9528183281409af0e7a0b5e6b33b33190c23d7c
fawadaslam77 <fwadaslam77@gmail.com>
2024-06-02 22:32:41 +0500

    Fix create template

--- app/api/template/route.ts
+++ app/api/template/route.ts
@@ -44,13 +44,52 @@ export async function POST(request: NextRequest) {
         return NextResponse.json({ success: false, message: 'Please provide required fields.' }, { status: 404 })
     }
     try {
+
+        // TODO For now we have table for one category of template campings table we need to check based on template_category from which table to read 
+        // default data from and in which to insert
+
         const slug = template_name.toLowerCase().split(" ").join("-");
-        // RETURNING * gives the inserted row
+        // RETURNING * gives the inserted row so we can return data and check which category is inserted
         const res = await sql`
-      INSERT INTO campaign_templates (template_name, template_category, slug, created_by)
-      VALUES (${template_name}, ${template_category},${slug}, ${userId})
-      RETURNING *
-    `;
+        INSERT INTO campaign_templates (template_name, template_category, slug, created_by)
+        VALUES (${template_name}, ${template_category},${slug}, ${userId})
+        RETURNING * `;
+
+        // Create insert query
+        // Get col names to insert by default sorting
+        const col_names = await sql`SELECT column_name
+        FROM information_schema.columns
+        WHERE table_name = 'campaign_templates_data' order by ordinal_position ;`;
+
+        // Exclude first index as it is primary key
+        const insert_cols = col_names.rows.slice(1).map(item => item.column_name).join(', ');
+        let insert_str = `INSERT INTO campaign_templates_data (${insert_cols}) VALUES `
+        console.log(insert_str)
+
+        // Get default template data
+        const cols_to_fetch = col_names.rows.slice(2).map(item => item.column_name).join(', ');
+        const defaultTemplateData = await sql.query(`
+        SELECT ${cols_to_fetch} FROM campaign_templates_data
+        WHERE template_id = 0 ;`, []);
+        defaultTemplateData.rows.forEach((element, index) => {
+            insert_str += `(${res.rows[0].campaign_templates_id}, `;
+            const obj = Object.values(element)
+            obj.forEach((ele, ele_index) => {
+                // Last index
+                insert_str += ele ? "'" + ele + "'" : null;
+                if (ele_index !== obj.length - 1) {
+                    insert_str += ', '
+                }
+            })
+            insert_str += ')'
+            if (index !== defaultTemplateData.rows.length - 1) {
+                insert_str += ', '
+            }
+        });
+
+        //  May be handle here if template_data not inserted then delete then delete template ? 
+        const insertDefaultTemplate = await sql.query(insert_str, []);
+
         return NextResponse.json({ success: true, message: 'Template created successfully.', data: res.rows }, { status: 200 })
     } catch (error) {
         console.log(error)
