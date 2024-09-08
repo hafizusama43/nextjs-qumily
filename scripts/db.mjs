34b9897b485e0dd9106883cb9cf739fd1fe7e19e
fawadaslam77 <fwadaslam77@gmail.com>
2024-09-08 13:20:29 +0500

    Fix script issues to close connections

--- scripts/db.mjs
+++ scripts/db.mjs
@@ -1,12 +1,11 @@
 import { db } from "@vercel/postgres";
 import pkg from 'pg';
-const { Client } = pkg;
+const { Pool } = pkg; // Use Pool instead of Client
 
-
-const DbConnect = async () => {
+const PgSqlPool = async () => {
     let client;
     if (process.env.NEXT_ENV === 'development') {
-        client = new Client({
+        client = new Pool({
             user: 'postgres',
             password: 'postgres',
             host: 'localhost',
@@ -14,11 +13,11 @@ const DbConnect = async () => {
             database: 'taskify',
         });
     } else {
-        client = db; // Use the imported Vercel Postgres client options
+        client = db; // Use the imported Vercel Postgres pool
     }
 
     try {
-        await client.connect();
+        // Pool doesn't require a connect call, the query will handle it
         console.info(`Connected to ${process.env.NODE_ENV === 'development' ? 'local' : 'Vercel'} PostgreSQL database`);
         return client;
     } catch (err) {
@@ -27,4 +26,4 @@ const DbConnect = async () => {
     }
 };
 
-export { DbConnect };
+export { PgSqlPool };
